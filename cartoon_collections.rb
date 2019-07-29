7840ec0be8d37c78232a6c339f1ffdf41fb63b84
Patrick Harrison <jahman@bellatlantic.net>
2019-07-29 23:50:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  [dwarves].each_with_index {|val,index|puts "#{index+1}.*#{val"}
+  [dwarves].each_with_index {|val,index|puts "#{index+1}.#{val"}
    end
 
 def summon_captain_planet(fruits)

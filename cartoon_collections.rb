ba2de759894b05dd85cfb05ef664ecaa2ce1ee53
Patrick Harrison <jahman@bellatlantic.net>
2019-07-30 00:59:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,17 @@
 def roll_call_dwarves(dwarves)# code an argument here
    dwarves.each_with_index { |val, idx| puts "#{idx+1}.#{val"}
 end
-def summon_captain_planet(fruits)
-  # code an argument here
-  # Your code here
+# def summon_captain_planet(fruits)
+#   # code an argument here
+#   # Your code here
  
-end
+# end
 
-def long_planeteer_calls()# code an argument here
-  # Your code here
-end
+# def long_planeteer_calls()# code an argument here
+#   # Your code here
+# end
 
-def find_the_cheese()# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+# def find_the_cheese()# code an argument here
+#   # the array below is here to help
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
